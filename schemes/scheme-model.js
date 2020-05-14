d33b7c6b4cfcc2911b35a1cd7e20185be88fe1ff
Anthony K <antonyk@users.noreply.github.com>
2020-05-14 15:57:55 -0400

    model methods implementation

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,3 +1,5 @@
+const db = require('../data/dbContext');
+
 module.exports = {
   find,
   findById,
@@ -7,16 +9,23 @@ module.exports = {
   remove
 }
 
-function find() {
+const schemes = db('schemes');
 
+function find() {
+  return db('schemes');
 }
 
 function findById(id) {
-
+  return db('schemes')
+    .where({id})
+    .first()
 }
 
 function findSteps(id) {
-
+  return db.select({id: 'a.id', step: 'a.step_number', instructions: 'a.instructions', scheme: 'schemes.scheme_name'})
+    .from({a: 'steps'})
+    .join('schemes', 'a.scheme_id', 'schemes.id')
+    .where('a.scheme_id', id);
 }
 
 function add(scheme) {
@@ -28,5 +37,5 @@ function update(changes, id) {
 }
 
 function remove(id) {
-  
+
 }
